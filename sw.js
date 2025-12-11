442b43b9a98e6474a404030c97caabaee8b6a6d6
walthereickeVV <walthereicke666@gmail.com>
2025-12-11 04:55:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
 const CACHE_NAME = 'radko-pharm-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap'
-];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                console.log('Cache opened');
+                return Promise.resolve();
+            })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
 
